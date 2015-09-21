f16095f2dee3099000cf56b8289521c91ee62b43
Evgeniy <yavorskiy1991@inbox.ru>
2015-09-21 20:30:27 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(function() {
-	$.ajax({
+
+	$('button').on('click', function(){
+		$.ajax({
 		url: 'http://theoldnewbie.github.io/ajax_test_edc/new_index.html',
 		success: function(data){
 			alert('Load was performed.');
 
 			$('.main-content').html(data);
-		}
+		};
 	});
+	});
+	
 });
