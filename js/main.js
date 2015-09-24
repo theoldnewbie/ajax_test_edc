11ef308a9fccf2a9ddb49e27d9d6120c1820b6ea
Vladislav <c00per@mail.ua>
2015-09-24 11:48:53 +0300

    test1

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
 
 
-	$('b1').on('click', function(){
-		$.ajax({
-		url: 'http://theoldnewbie.github.io/ajax_test_edc/home.html',
-		success: function(data){
-			alert('Home');
+	$('button').on('click', function(){
+		var id = jQuery.data(id);
+		if(id = 'b1'){
+			$.ajax({
+			url: 'http://theoldnewbie.github.io/ajax_test_edc/home.html',
+			success: function(data){
+				alert('Home');
 
-			$('.main-content').html(data);
+				$('.main-content').html(data);
+			}
+		});
+		}else if(id = 'b2'){
+			$.ajax({
+			url: 'http://theoldnewbie.github.io/ajax_test_edc/books.html',
+			success: function(data){
+				alert('Books');
+
+				$('.main-content').html(data);
+			}
+		});
+		}else if(id = 'b3'){
+			$.ajax({
+			url: 'http://theoldnewbie.github.io/ajax_test_edc/authors.html',
+			success: function(data){
+				alert('Authors');
+
+				$('.main-content').html(data);
+			}
+		});
 		}
-	});
 	})
 
-	$('b2').on('click', function(){
-		$.ajax({
-		url: 'http://theoldnewbie.github.io/ajax_test_edc/books.html',
-		success: function(data){
-			alert('Books');
-
-			$('.main-content').html(data);
-		}
-	});
-	})
-
-	$('b3').on('click', function(){
-		$.ajax({
-		url: 'http://theoldnewbie.github.io/ajax_test_edc/authors.html',
-		success: function(data){
-			alert('Authors');
-
-			$('.main-content').html(data);
-		}
-	});
-	})
 	
-
 
 // $(button).click(function() {
 //     var id = jQuery.data(id);
