94f2b41d6887e4a72ee160a15ecc8cf08b080d95
Vladislav <c00per@mail.ua>
2015-09-24 11:44:00 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 
 
-	$('button').on('click', function(){
+	$('b1').on('click', function(){
 		$.ajax({
 		url: 'http://theoldnewbie.github.io/ajax_test_edc/home.html',
 		success: function(data){
-			alert('Load was performed.');
+			alert('Home');
+
+			$('.main-content').html(data);
+		}
+	});
+	})
+
+	$('b2').on('click', function(){
+		$.ajax({
+		url: 'http://theoldnewbie.github.io/ajax_test_edc/books.html',
+		success: function(data){
+			alert('Books');
+
+			$('.main-content').html(data);
+		}
+	});
+	})
+
+	$('b3').on('click', function(){
+		$.ajax({
+		url: 'http://theoldnewbie.github.io/ajax_test_edc/authors.html',
+		success: function(data){
+			alert('Authors');
 
 			$('.main-content').html(data);
 		}
 	});
 	})
 	
+
+
+// $(button).click(function() {
+//     var id = jQuery.data(id);
+//     function send()
+//     {
+//        $.ajax({
+//                 type: "POST",
+//                 url: "/change.php",
+//                 data: "b"+id,
+                
+//                 success: function(html) {
+//                         $(".r"+id).empty();
+//                         $(".r"+id).append(html);
+//                 }
+//         });
+ 
+//     }
+ 
+// });
